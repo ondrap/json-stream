302329be5f718fa9b4c6cda600c28ca138d85284
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2023-11-20 10:54:12 +0100

    Updated benchamrk to use use aeson-2.2. Aeson does strings slightly better.

--- benchmarks/AesonParse.hs
+++ benchmarks/AesonParse.hs
@@ -1,19 +1,19 @@
 {-# LANGUAGE BangPatterns, OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Control.Exception
 import Control.Monad
 import Data.Aeson
-import Data.Attoparsec (IResult(..), parseWith)
 import Data.Time.Clock
 import System.Environment (getArgs)
 import System.IO
 import qualified Data.ByteString as B
+import Control.DeepSeq (deepseq)
 
 main :: IO ()
 main = do
   (bs:cnt:args) <- getArgs
   let count = read cnt :: Int
-      blkSize = read bs
   forM_ args $ \arg -> bracket (openFile arg ReadMode) hClose $ \h -> do
     putStrLn $ arg ++ ":"
     start <- getCurrentTime
@@ -21,11 +21,11 @@ main = do
             | good+bad >= count = return (good, bad)
             | otherwise = do
           hSeek h AbsoluteSeek 0
-          let refill = B.hGet h blkSize
-          result <- parseWith refill json =<< refill
-          case result of
-            Done _ _ -> loop (good+1) bad
-            _        -> loop good (bad+1)
+          content <- B.hGet h 4200000
+          let result = decodeStrict' content
+          case result `deepseq` result of
+            Just (_ :: Value)  -> loop (good+1) bad
+            Nothing -> loop good (bad+1)
     (good, _) <- loop 0 0
     delta <- flip diffUTCTime start `fmap` getCurrentTime
     putStrLn $ "  " ++ show good ++ " good, " ++ show delta
